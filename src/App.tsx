fbc7e64b29f9dbeaca49add4d6f8936cf1c30e20
Hunter Horton <hhorton@atlasrfid.com>
2021-10-21 16:50:26 -0500

    first build

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
-    AppBar,
-    Box,
-    IconButton,
-    List,
-    Toolbar,
-    Typography,
+  AppBar,
+  Box,
+  IconButton,
+  List,
+  Toolbar,
+  Typography,
 } from "@mui/material";
-import {styled, useTheme} from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import Drawer from "@mui/material/Drawer";
 import MenuIcon from "@mui/icons-material/Menu";
 import PostOpDiet from "./components/PostOpDiet/PostOpDiet";
@@ -18,91 +18,91 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 
-const DrawerHeader = styled("div")(({theme}) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
 }));
 
 function App() {
-    const theme = useTheme();
-    const [open, setOpen] = useState(false);
-    const postOpDietName = "Post-op Diet";
-    const [openPage, setOpenPage] = useState(postOpDietName);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const postOpDietName = "Post-op Diet";
+  const [openPage, setOpenPage] = useState(postOpDietName);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{mr: 2}}
-                        onClick={handleDrawerOpen}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        {openPage}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                sx={{
-                    width: 240,
-                    flexShrink: 0,
-                    "& .MuiDrawer-paper": {
-                        width: 240,
-                        boxSizing: "border-box",
-                    },
-                }}
-                variant="persistent"
-                anchor="left"
-                open={open}
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {openPage}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 240,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <Typography>Navigation</Typography>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {[postOpDietName].map((text, index) => (
+            <ListItem
+              button
+              key={text}
+              onClick={() => {
+                setOpenPage(text);
+                setOpen(false);
+              }}
             >
-                <DrawerHeader>
-                    <Typography>Navigation</Typography>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === "ltr" ? (
-                            <ChevronLeftIcon/>
-                        ) : (
-                            <ChevronRightIcon/>
-                        )}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider/>
-                <List>
-                    {[postOpDietName].map((text, index) => (
-                        <ListItem
-                            button
-                            key={text}
-                            onClick={() => {
-                                setOpenPage(text);
-                                setOpen(false);
-                            }}
-                        >
-                            <ListItemText primary={text}/>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-            <div style={{marginTop: 5}}>
-                {openPage === postOpDietName && <PostOpDiet/>}
-            </div>
-        </Box>
-    );
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <div style={{ marginTop: 5 }}>
+        {openPage === postOpDietName && <PostOpDiet />}
+      </div>
+    </Box>
+  );
 }
 
 export default App;
